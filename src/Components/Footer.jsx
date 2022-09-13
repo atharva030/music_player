734716dbe258cf149f0c2481c2e079f3c711b3f7
atharva030 <atharvajagdale28@gmail.com>
2022-09-13 22:10:03 +0530

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import '../Styles/footer.css'
 const Footer = () => {
   return (
     <footer>
-  <p style={{textAlign:"center"}}>Copyright &#169; 2022 | Atharva Jagdale<br/>
+  <p style={{textAlign:"center",marginLeft: "10rem"}}>Copyright &#169; 2022 | Atharva Jagdale<br/>
   <a href="mailto:hege@example.com">atharvajagdale28@gmail.com</a></p>
 </footer>
 
--- src/Components/Songlist.jsx
+++ src/Components/Songlist.jsx
@@ -211,6 +211,7 @@ const Songlist = ({ handlePlaylist,handleFavorites }) => {
           })}
         </Slider>
       </div>
+      
     </>
   );
 };
